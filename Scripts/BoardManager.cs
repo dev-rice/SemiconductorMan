71724a898e9703a04987fa21c4fe39219ad00497
Chris Rice <chrisrice93@gmail.com>
2015-04-26 21:19:43 -0600

    Added the spritesheet and electron and hole prefabs.

---
+++ Scripts/BoardManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+	private GameObject[] point_bubbles;
+
+	public void setupBoard(){
+		// Create the path structure
+
+		// Add point bubbles to the path
+	}
+
+	public void createPaths(){
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
