a89b9620d16b947f0a2c1f59600cac38df5a3913
Chris Rice <chrisrice93@gmail.com>
2015-04-28 01:31:15 -0600

    *finished* the graph class.

--- Scripts/BoardManager.cs
+++ Scripts/BoardManager.cs
@@ -13,12 +13,6 @@ public class BoardManager : MonoBehaviour {
 	public void setupBoard(){
 		boardHolder = new GameObject("Board").transform;
 
-		// // Create the point bubbles
-		// for (int x = 0; x < 10; ++x){
-		// 	GameObject instance = Instantiate(point_bubble, new Vector3(bubble_spacing * x, 0, 0), Quaternion.identity) as GameObject;
-		// 	instance.transform.SetParent(boardHolder);
-		// }
-
 		for (int i = 0; i < graph.test_path.points.Count; ++i){
 			Vector2 current_point = graph.test_path.points[i];
 			GameObject instance = Instantiate(point_bubble, new Vector3(current_point.x, current_point.y, 0), Quaternion.identity) as GameObject;
--- Scripts/Graph.cs
+++ Scripts/Graph.cs
@@ -22,16 +22,13 @@ public class GamePath {
 
 		// Given node a and b, and a step size, create a list of points representing the path between the two nodes.
 		float t = 0;
-
-		Debug.Log("Creating path between " + a.position + " and " + b.position);
-
 		while (t < 1){
 			Vector2 current_point = thirdOrderBezier(a.position, a.position + new Vector2(1, 0), b.position - new Vector2(1, 0), b.position, t);
 			points.Add(current_point);
 			t += step_size;
 		}
 		// Create point for t=1
-		Vector2 last_point = thirdOrderBezier(a.position, a.position + new Vector2(1, 0), b.position - new Vector2(1, 0), b.position, 1);
+		Vector2 last_point = thirdOrderBezier(a.position, a.position, b.position, b.position, 1);
 		points.Add(last_point);
 
 	}
@@ -53,20 +50,99 @@ public class GamePath {
 }
 
 [Serializable]
-public class Graph {
-	private Dictionary<string, Node> nodes = new Dictionary<string, Node>();
-	private Dictionary<Node, List<Node>> adj_list = new Dictionary<Node, List<Node>>();
+public class AdjacencyList {
+	public Dictionary<Node, List<Node>> map = new Dictionary<Node, List<Node>>();
 
+	public AdjacencyList(){
+
+	}
+
+	public void addUndirectedEdge(Node a, Node b){
+		if (!map.ContainsKey(a)){
+			map.Add(a, new List<Node>());
+		}
+
+		if (!map[a].Contains(b)){
+			map[a].Add(b);
+		}
+
+		if (!map.ContainsKey(b)){
+			map.Add(b, new List<Node>());
+		}
+
+		if (!map[b].Contains(a)){
+			map[b].Add(a);
+		}
+
+	}
+
+	public List<Node> getAdjacentNodes(Node a){
+		return map[a];
+	}
+}
+
+[Serializable]
+public class PathHolder {
+	private Dictionary<Node, Dictionary<Node, GamePath>> path_map = new Dictionary<Node, Dictionary<Node, GamePath>>();
+
+	public PathHolder(AdjacencyList adj_list){
+		// Create all of the paths in the adjacency list
+		foreach(KeyValuePair<Node, List<Node>> entry in adj_list.map){
+			foreach(Node node in entry.Value){
+				addUndirectedPath(entry.Key, node);
+			}
+		}
+	}
+
+	public void addUndirectedPath(Node a, Node b){
+		GamePath path = new GamePath(a, b, 10);
+
+		// Create the path if the path hasn't been added already
+		if (!path_map.ContainsKey(a)){
+			path_map.Add(a, new Dictionary<Node, GamePath>());
+		}
+		if (!path_map[a].ContainsKey(b)){
+			path_map[a].Add(b, path);
+		}
+
+		if (!path_map.ContainsKey(b)){
+			path_map.Add(b, new Dictionary<Node, GamePath>());
+		}
+		if (!path_map[b].ContainsKey(a)){
+			path_map[b].Add(a, path);
+		}
+
+	}
+
+	public GamePath getPathBetween(Node a, Node b){
+		return path_map[a][b];
+	}
+
+}
+
+[Serializable]
+public class Graph {
+
+	public AdjacencyList adj_list = new AdjacencyList();
+	public PathHolder path_holder;
 	public GamePath test_path;
 
 	public Graph(){
-		// Create an example graph
-		Node a = new Node(new Vector2(-4.0f, -2.0f));
-		Node b = new Node(new Vector2(-2.0f, -2.0f));
-		Node c = new Node(new Vector2(2.0f, 2.0f));
-		Node d = new Node(new Vector2(4.0f, 2.0f));
+		// Create an example graph with adjacency list
+		Node a = new Node(new Vector2(-4.0f, 4.0f));
+		Node b = new Node(new Vector2(4.0f, 4.0f));
+		Node c = new Node(new Vector2(-4.0f, -4.0f));
+		Node d = new Node(new Vector2(4.0f, -4.0f));
 
-		test_path = new GamePath(b, c, 10);
+		adj_list.addUndirectedEdge(a, b);
+		adj_list.addUndirectedEdge(a, c);
+		adj_list.addUndirectedEdge(b, d);
+		adj_list.addUndirectedEdge(c, d);
+
+		// Create the paths from the adjacency list
+		path_holder = new PathHolder(adj_list);
+
+		test_path = path_holder.getPathBetween(c, d);
 
 	}
 
