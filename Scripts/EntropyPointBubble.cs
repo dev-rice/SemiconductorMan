edcde849f241066dac686853347334b321b705dd
Chris Rice <chrisrice93@gmail.com>
2015-04-28 18:55:32 -0600

    Made some of the entropy points play backwards at random.

---
+++ Scripts/EntropyPointBubble.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class EntropyPointBubble : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+
+		int randnum = Random.Range(0, 2);
+		bool is_reversed = (randnum == 1);
+		animator.SetBool("reversed", is_reversed);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
